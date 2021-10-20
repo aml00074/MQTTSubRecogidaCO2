bc8a6086542475fa7d98298a11dbecf928ed11ac
Alicia Montoro Lendínez <aml00074@red.ujaen.es>
2021-10-20 13:31:24 +0200

    second commit

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -4,14 +4,16 @@ broker_address = "192.168.196.139"
 broker_port = 1883
 topic = "medida"
 
+
 def on_message(client, userdata, message):
- print("Mensaje recibido=", str(message.payload.decode("utf-8")))
- print("Topic=", message.topic)
- print("Nivel de calidad [0|1|2]=", message.qos)
- print("Flag de retención =", message.retain)
+    print("Mensaje recibido=", str(message.payload.decode("utf-8")))
+    print("Topic=", message.topic)
+    print("Nivel de calidad [0|1|2]=", message.qos)
+    print("Flag de retención =", message.retain)
+
 
 client = mqtt.Client('Cliente1')
 client.on_message = on_message
 client.connect(broker_address, broker_port, 60)
-client.subscribe(topic) # Subscripción al topic
+client.subscribe(topic)  # Subscripción al topic
 client.loop_forever()
---
+++ EjemploMQTT2.py
@@ -0,0 +1,13 @@
+import paho.mqtt.client as mqtt
+import time
+
+broker_address = "192.168.196.139"
+
+client = mqtt.Client('Publicador1')  # Creación del cliente
+client.connect(broker_address)
+topic = "led"
+
+client.publish(topic, "0")
+time.sleep(4)
+client.publish(topic, "1")
+client.loop_forever()
