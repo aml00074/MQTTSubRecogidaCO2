946fd6911464a185069dca84a53c0a5b804ed5a9
Alicia Montoro Lendínez <aml00074@red.ujaen.es>
2021-10-22 12:03:48 +0200

    eight commit

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -2,29 +2,37 @@ import paho.mqtt.client as mqtt
 import pytz
 import datetime
 import json
+import pymongo
+
 broker_address = "192.168.195.139"
 broker_port = 1883
 topic = "medida"
-dict = {}
-dict['CO2'] = []
-# print(pytz.all_timezones) #España es 'Europe/Madrid',
+lista = []
+# MongoDB
+uri = 'mongodb://admin:qz3qGzXvu6mZjPkiJ6@asia.ujaen.es:8047/?authSource=admin&authMechanism=SCRAM-SHA-256'
+myclient = pymongo.MongoClient(uri)
+mydb = myclient["Ejemplo"]
+mycol = mydb["samplesCO2"]
 
 def on_message(client, userdata, message):
     print("Mensaje recibido=", str(message.payload.decode("utf-8")))
     print("Topic=", message.topic)
     print("Nivel de calidad [0|1|2]=", message.qos)
     print("Flag de retención =", message.retain)
-    print("Timestamp=",datetime.datetime.now(pytz.timezone('Europe/Madrid')))
-    timestamp = datetime.datetime.now(pytz.timezone('Europe/Madrid'))
-    dict['CO2'].append({
-        'medidaA0': str(message.payload.decode("utf-8")),
-        'timestamp': [timestamp.day, timestamp.month, timestamp.year, timestamp.hour, timestamp.minute,
-                      timestamp.second]
-    })
-    print(dict)
-    with open('dict.json', 'w') as file:
-        json.dump(dict, file, indent=4)
 
+    timestamp = datetime.datetime.now(pytz.timezone('Europe/Madrid'))
+    timestamp_str = timestamp.strftime("%d/%m/%Y, %H:%M:%S")
+    print("Timestamp=",timestamp_str)
+
+    '''dict.append({
+        'medidaA0': float(str(message.payload.decode("utf-8"))),
+        'timestamp': timestamp_str
+    })'''
+    list= [{
+        'medidaA0': float(str(message.payload.decode("utf-8"))),
+        'timestamp': timestamp_str
+    }]
+    mycol.insert_many(list)
 
 
 client = mqtt.Client('Cliente1')
