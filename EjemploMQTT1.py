ba21e8e7541e45133856c5e6bf1e3877cf8a2b24
Alicia Montoro Lendínez <aml00074@red.ujaen.es>
2021-10-22 10:01:40 +0200

    seven commit

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -1,15 +1,30 @@
 import paho.mqtt.client as mqtt
-
-broker_address = "192.168.196.139"
+import pytz
+import datetime
+import json
+broker_address = "192.168.195.139"
 broker_port = 1883
 topic = "medida"
-
+dict = {}
+dict['CO2'] = []
+# print(pytz.all_timezones) #España es 'Europe/Madrid',
 
 def on_message(client, userdata, message):
     print("Mensaje recibido=", str(message.payload.decode("utf-8")))
     print("Topic=", message.topic)
     print("Nivel de calidad [0|1|2]=", message.qos)
     print("Flag de retención =", message.retain)
+    print("Timestamp=",datetime.datetime.now(pytz.timezone('Europe/Madrid')))
+    timestamp = datetime.datetime.now(pytz.timezone('Europe/Madrid'))
+    dict['CO2'].append({
+        'medidaA0': str(message.payload.decode("utf-8")),
+        'timestamp': [timestamp.day, timestamp.month, timestamp.year, timestamp.hour, timestamp.minute,
+                      timestamp.second]
+    })
+    print(dict)
+    with open('dict.json', 'w') as file:
+        json.dump(dict, file, indent=4)
+
 
 
 client = mqtt.Client('Cliente1')
