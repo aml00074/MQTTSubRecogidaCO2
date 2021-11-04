2f977df12fddbf885bde1d9b894a4f210ea91249
aml00074 <aml00074@red.ujaen.es>
2021-11-04 13:52:30 +0100

    Update EjemploMQTT1.py

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -26,6 +26,8 @@ def on_message(client, userdata, message):
     timestamp = datetime.datetime.now(pytz.timezone('Europe/Madrid'))
     timestamp_str = timestamp.strftime("%d/%m/%Y, %H:%M:%S")
     print("Timestamp=",timestamp_str)
+    separados=str(message.payload.decode("utf-8")).split(' ')
+    print(separados)
 
     '''dict.append({
         'medidaA0': float(str(message.payload.decode("utf-8"))),
