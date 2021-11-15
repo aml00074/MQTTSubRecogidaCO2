41595e2715576d05b328af8d663b90573e1607d3
aml00074 <aml00074@red.ujaen.es>
2021-11-15 12:29:11 +0100

    Update EjemploMQTT1.py

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -6,22 +6,23 @@ import datetime
 import pymongo
 import json
 
-ACCESS_TOKEN="holacocacoli123"
+
 broker_address = "192.168.1.150"
 broker_port = 1883
-topic = "v1/devices/me/telemetry"
+topic = "medida"
 lista = []
 # MongoDB
 uri = 'mongodb://admin:qz3qGzXvu6mZjPkiJ6@asia.ujaen.es:8047/?authSource=admin&authMechanism=SCRAM-SHA-256'
 myclient = pymongo.MongoClient(uri)
 mydb = myclient["Ejemplo2"]
 mycol = mydb["samplesMQ135"]
+print('hi')
 '''for x in mycol.find().sort("timestamp", -1):
     print(x)'''
 
 def on_message(client, userdata, message):
     print('hi3')
-    mensaje=message.payload.decode("utf-8")
+    mensaje=str(message.payload.decode("utf-8"))
     print("Mensaje recibido=",str(message.payload.decode("utf-8")))
     print("Topic=", message.topic)
     print("Nivel de calidad [0|1|2]=", message.qos)
@@ -30,36 +31,30 @@ def on_message(client, userdata, message):
     timestamp = datetime.datetime.now(pytz.timezone('Europe/Madrid'))
     timestamp_str = timestamp.strftime("%d/%m/%Y, %H:%M:%S")
     print("Timestamp=",timestamp_str)
-    separado=json.load(mensaje)
-    #separado=mensaje.split(':')
+    separado=mensaje.split(';')
     print(separado)
 
     '''dict.append({
         'medidaA0': float(str(message.payload.decode("utf-8"))),
         'timestamp': timestamp_str
     })'''
-    list = [{
-        'medidaCO2': 1,
-        'timestamp': timestamp_str
-    }]
-    '''list= [{
-        'medidaCO2':separado[0],
-        'medidaCO':separado[1],
+    list= [{
+        'medidaCO2':float(separado[0]),
+        'medidaCO':float(separado[1]),
         'medidaNH4':separado[2],
         'medidaAlcohol':separado[3],
         'medidaAcetona':separado[4],
         'timestamp': timestamp_str
-    }]'''
+    }]
     mycol.insert_many(list)
 
 while(True):
     try:
         client = mqtt.Client('Cliente1')
         client.on_message = on_message
-        client.username_pw_set(ACCESS_TOKEN)
         client.connect(broker_address, broker_port, 60)
         print('hi1.5')
-        client.subscribe("v1/devices/me/telemetry" % ACCESS_TOKEN,1)  # Subscripción al topic
+        client.subscribe(topic)  # Subscripción al topic
         print('hi2')
         client.loop_forever()
         print('hi4')
