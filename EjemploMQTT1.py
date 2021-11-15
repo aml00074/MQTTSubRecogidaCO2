7cc8396f94ae47355322c0f22eb883dcda3db827
aml00074 <aml00074@red.ujaen.es>
2021-11-15 09:59:02 +0100

    Update EjemploMQTT1.py

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -9,20 +9,19 @@ import json
 
 broker_address = "192.168.1.150"
 broker_port = 1883
-topic = "#"
+topic = "v1/devices/telemetry"
 lista = []
 # MongoDB
 uri = 'mongodb://admin:qz3qGzXvu6mZjPkiJ6@asia.ujaen.es:8047/?authSource=admin&authMechanism=SCRAM-SHA-256'
 myclient = pymongo.MongoClient(uri)
 mydb = myclient["Ejemplo2"]
 mycol = mydb["samplesMQ135"]
-print('hi')
 '''for x in mycol.find().sort("timestamp", -1):
     print(x)'''
 
 def on_message(client, userdata, message):
     print('hi3')
-    mensaje=str(message.payload.decode("utf-8"))
+    mensaje=message.payload.decode("utf-8")
     print("Mensaje recibido=",str(message.payload.decode("utf-8")))
     print("Topic=", message.topic)
     print("Nivel de calidad [0|1|2]=", message.qos)
@@ -31,7 +30,8 @@ def on_message(client, userdata, message):
     timestamp = datetime.datetime.now(pytz.timezone('Europe/Madrid'))
     timestamp_str = timestamp.strftime("%d/%m/%Y, %H:%M:%S")
     print("Timestamp=",timestamp_str)
-    #separado=mensaje.split(';')
+    separado=json.load(mensaje)
+    #separado=mensaje.split(':')
     #print(separado)
 
     '''dict.append({
@@ -39,11 +39,7 @@ def on_message(client, userdata, message):
         'timestamp': timestamp_str
     })'''
     list = [{
-        'medidaCO2': 0,
-        'medidaCO': 1,
-        'medidaNH4': 2,
-        'medidaAlcohol': 3,
-        'medidaAcetona':4,
+        'medidaCO2': separado['CO2'],
         'timestamp': timestamp_str
     }]
     '''list= [{
