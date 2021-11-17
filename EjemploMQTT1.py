437a673d391d9f5f249d1538cd12ff49db1f7bae
aml00074 <aml00074@red.ujaen.es>
2021-11-17 09:03:40 +0100

    Update EjemploMQTT1.py

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import pytz
 import datetime
 import pymongo
@@ -9,7 +10,8 @@ import json
 
 broker_address = "192.168.1.150"
 broker_port = 1883
-topic = "medida"
+topic_sub = "medida"
+topic_pub1 = "CO2"
 lista = []
 # MongoDB
 uri = 'mongodb://admin:qz3qGzXvu6mZjPkiJ6@asia.ujaen.es:8047/?authSource=admin&authMechanism=SCRAM-SHA-256'
@@ -20,10 +22,9 @@ mycol = mydb["samplesMQ135"]
     print(x)'''
 
 def on_message(client, userdata, message):
-    print('hi3')
     mensaje=str(message.payload.decode("utf-8"))
     print("Mensaje recibido=",str(message.payload.decode("utf-8")))
-    print("Topic=", message.topic)
+    print("Topic=", message.topic_sub)
     print("Nivel de calidad [0|1|2]=", message.qos)
     print("Flag de retención =", message.retain)
 
@@ -31,6 +32,10 @@ def on_message(client, userdata, message):
     timestamp_str = timestamp.strftime("%d/%m/%Y, %H:%M:%S")
     print("Timestamp=",timestamp_str)
     separado=mensaje.split(';')
+    #Publicación de las medidas
+    clientp=mqtt.Client('python_publish')
+    clientp.connect(broker_address, broker_port, 60)
+    clientp.publish('C02', separado[0])
 
     '''dict.append({
         'medidaA0': float(str(message.payload.decode("utf-8"))),
@@ -51,7 +56,7 @@ while(True):
         client = mqtt.Client('Cliente1')
         client.on_message = on_message
         client.connect(broker_address, broker_port, 60)
-        client.subscribe(topic)  # Subscripción al topic
+        client.subscribe(topic_sub)  # Subscripción al topic_sub
         client.loop_forever()
         ''' para poder ordenar dentro de MongoDB db.getCollection('samplesCO2').find({}).sort({timestamp : 1})'''
     except Exception as e:
