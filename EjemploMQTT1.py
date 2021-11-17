c018628f7c539dd62d5c34e4be62400224bf13a0
aml00074 <aml00074@red.ujaen.es>
2021-11-17 09:31:00 +0100

    Update EjemploMQTT1.py

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -1,17 +1,19 @@
-from typing import List
-
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import pytz
 import datetime
 import pymongo
-import json
+
 
 
 broker_address = "192.168.1.150"
 broker_port = 1883
 topic_sub = "medida"
-topic_pub1 = "CO2"
+topic_pub0 = "recogida/CO2"
+topic_pub1 = "recogida/CO"
+topic_pub2 = "recogida/NH4"
+topic_pub3 = "recogida/Alcohol"
+topic_pub4 = "recogida/Acetona"
 lista = []
 # MongoDB
 uri = 'mongodb://admin:qz3qGzXvu6mZjPkiJ6@asia.ujaen.es:8047/?authSource=admin&authMechanism=SCRAM-SHA-256'
@@ -24,7 +26,7 @@ mycol = mydb["samplesMQ135"]
 def on_message(client, userdata, message):
     mensaje=str(message.payload.decode("utf-8"))
     print("Mensaje recibido=",str(message.payload.decode("utf-8")))
-    print("Topic=", message.topic_sub)
+    print("Topic=", message.topic)
     print("Nivel de calidad [0|1|2]=", message.qos)
     print("Flag de retención =", message.retain)
 
@@ -32,21 +34,21 @@ def on_message(client, userdata, message):
     timestamp_str = timestamp.strftime("%d/%m/%Y, %H:%M:%S")
     print("Timestamp=",timestamp_str)
     separado=mensaje.split(';')
-    #Publicación de las medidas
-    clientp=mqtt.Client('python_publish')
-    clientp.connect(broker_address, broker_port, 60)
-    clientp.publish('C02', separado[0])
-
+    publish.single(topic_pub0, separado[0], hostname="192.168.1.150")
+    publish.single(topic_pub1, separado[1], hostname="192.168.1.150")
+    publish.single(topic_pub2, separado[2], hostname="192.168.1.150")
+    publish.single(topic_pub3, separado[3], hostname="192.168.1.150")
+    publish.single(topic_pub4, separado[4], hostname="192.168.1.150")
     '''dict.append({
         'medidaA0': float(str(message.payload.decode("utf-8"))),
         'timestamp': timestamp_str
     })'''
     list= [{
-        'medidaCO2':separado [0],
-        'medidaCO':separado [1],
-        'medidaNH4':separado [2],
-        'medidaAlcohol':separado [3],
-        'medidaAcetona':separado [4],
+        'medidaCO2':separado[0],
+        'medidaCO':separado[1],
+        'medidaNH4':separado[2],
+        'medidaAlcohol':separado[3],
+        'medidaAcetona':separado[4],
         'timestamp': timestamp_str
     }]
     mycol.insert_many(list)
