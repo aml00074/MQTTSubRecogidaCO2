52ee591be4cc55d236a68b0ff3b186aeda5a87db
aml00074 <aml00074@red.ujaen.es>
2021-12-09 13:26:33 +0100

    Update EjemploMQTT1.py

--- EjemploMQTT1.py
+++ EjemploMQTT1.py
@@ -14,6 +14,8 @@ topic_pub1 = "recogida/CO"
 topic_pub2 = "recogida/NH4"
 topic_pub3 = "recogida/Alcohol"
 topic_pub4 = "recogida/Acetona"
+topic_pub5 = "recogida/etiquetado"
+
 lista = []
 # MongoDB
 uri = 'mongodb://admin:qz3qGzXvu6mZjPkiJ6@asia.ujaen.es:8047/?authSource=admin&authMechanism=SCRAM-SHA-256'
@@ -34,11 +36,13 @@ def on_message(client, userdata, message):
     timestamp_str = timestamp.strftime("%d/%m/%Y, %H:%M:%S")
     print("Timestamp=",timestamp_str)
     separado=mensaje.split(';')
+    print(separado)
     publish.single(topic_pub0, separado[1], hostname="192.168.1.150")
     publish.single(topic_pub1, separado[2], hostname="192.168.1.150")
     publish.single(topic_pub2, separado[3], hostname="192.168.1.150")
     publish.single(topic_pub3, separado[4], hostname="192.168.1.150")
     publish.single(topic_pub4, separado[5], hostname="192.168.1.150")
+    publish.single(topic_pub5, separado[0], hostname="192.168.1.150")
     print("Mensajes de las medidas recogidas publicados")
     '''dict.append({
         'medidaA0': float(str(message.payload.decode("utf-8"))),
